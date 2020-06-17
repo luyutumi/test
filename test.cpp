cbabc70b3a9cbfe3d4f84f816a6608c1aee93763
yulutumi <18340826652@163.com>
2020-06-17 18:18:40 +0800

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <memory>
+#include <vector>
+#include <set>
+#include <deque>
+#include <functional>
+#include <algorithm>
+#include <limits>
+#include <math.h>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "yl" << endl;
+
+	system("pause");
+	return 0;
+}
